be9df902169e361cf94e5de6c6fa8a3310c0c1e8
Harry Peaker <harry.peaker@aridhia.com>
2017-04-25 14:46:42 +0100

    remove unnecessary global assigns. currently locally assign where required

--- workspace/mini-apps/server.R
+++ workspace/mini-apps/server.R
@@ -3,11 +3,11 @@ library(shiny)
 data.maker <- function(data, y, x) {
   if(is.null(y)) res1=data[, 1] else res1=data[, y]
   if(is.null(x)) regr1=data[, 6] else regr1=data[, x]
-  x2 <<- regr1^2
-  x3 <<- regr1^3
-  sqrtx <<- sapply(regr1, function(x) {if(x > 0) sqrt(x) else 0})
-  logx <<- sapply(regr1, function(x) {if(x > 0) log(x) else -100})
-  expx <<- exp(regr1)
+  x2 <- regr1^2
+  x3 <- regr1^3
+  sqrtx <- sapply(regr1, function(x) {if(x > 0) sqrt(x) else 0})
+  logx <- sapply(regr1, function(x) {if(x > 0) log(x) else -100})
+  expx <- exp(regr1)
   data.frame(x=regr1,x2=x2,x3=x3,sqrtx=sqrtx,logx=logx,expx=expx,y=res1)
 }
 
@@ -17,30 +17,30 @@ server <- function(input, output, session) {
     # If missing input, return to avoid error later in function
     if(is.null(input$dataset))
       return()
-    dat <<- get(input$dataset)
-    columns <<- colnames(dat)
+    dat <- get(input$dataset)
+    columns <- colnames(dat)
     # Make sure columns are correct for data set (when data set changes, the
     # columns will initially be for the previous data set)
     if (is.null(columns) || !(columns %in% names(dat)))
       return()
     
-    res <<- input$result
+    res <- input$result
     #     
     # Make sure result is correct for data set (when data set changes, the
     # result will initially be for the previous data set)
     if (is.null(input$regressors) || !(input$regressors %in% names(dat)))
       return()
     
-    regr <<- input$regressors
+    regr <- input$regressors
     data.maker(data=dat, y=res, x=regr)
   })
   
   # Pick the resulting variable
   output$choose_result <- renderUI({
-    dat <<- get(input$dataset)
+    dat <- get(input$dataset)
     # Make sure columns are correct for data set (when data set changes, the
     # columns will initially be for the previous data set)
-    columns <<- colnames(dat)
+    columns <- colnames(dat)
     if (is.null(columns) || !(columns %in% names(dat)))
       return()
     selectInput("result","Pick resulting variable",
@@ -50,8 +50,8 @@ server <- function(input, output, session) {
   
   # Select the required regressors (Check boxes)
   output$choose_regressors <- renderUI({
-    dat <<- get(input$dataset)
-    columns <<- colnames(dat)
+    dat <- get(input$dataset)
+    columns <- colnames(dat)
     # Make sure columns are correct for data set (when data set changes, the
     # columns will initially be for the previous data set)
     if (is.null(columns) || !(columns %in% names(dat)))
@@ -67,20 +67,20 @@ server <- function(input, output, session) {
   })
   
   lmResults <- reactive({
-    regress.exp <<- input$regression
+    regress.exp <- input$regression
     if (!input$constant) regress.exp <- paste(input$regression, "- 1")
     
     lm(regress.exp, data=mydata())
   })
   
   output$lmStats <- renderTable({
-    
+    dat <- input$dataset
     # Make sure result is correct for data set (when data set changes, the
     # result will initially be for the previous data set)
     if (is.null(input$result) || !(input$result %in% names(dat)))
       return()
     
-    results <<- summary(lmResults())
+    results <- summary(lmResults())
     data.frame(R2=results$r.squared,
                adj.R2=results$adj.r.squared,
                DOF.model=results$df[1],
@@ -97,7 +97,7 @@ server <- function(input, output, session) {
   
   # Show coefficients
   output$lmResults <- renderTable({
-    
+    dat <- input$dataset
     # Make sure result is correct for data set (when data set changes, the
     # result will initially be for the previous data set)
     if (is.null(input$result) || !(input$result %in% names(dat)))
@@ -108,20 +108,20 @@ server <- function(input, output, session) {
   
   # Show plot of points, regression line, residuals
   output$scatter <- renderPlot({
-    data1 <<- mydata()
+    data1 <- mydata()
     if(length(data1) > 0){
-      x <<- data1$x
-      y <<- data1$y
+      x <- data1$x
+      y <- data1$y
       xcon <- seq(min(x)-.1, max(x)+.1, .025)
-      x2 <<- xcon^2
-      x3 <<- xcon^3
-      sqrtx <<- sapply(xcon, function(x) {if(x > 0) sqrt(x) else 0})
-      logx <<- sapply(xcon, function(x) {if(x > 0) log(x) else -100})
-      expx <<- exp(xcon)
+      x2 <- xcon^2
+      x3 <- xcon^3
+      sqrtx <- sapply(xcon, function(x) {if(x > 0) sqrt(x) else 0})
+      logx <- sapply(xcon, function(x) {if(x > 0) log(x) else -100})
+      expx <- exp(xcon)
       
-      predictor <<- data.frame(x=xcon,x2=x2,x3=x3,sqrtx=sqrtx,logx=logx,expx=expx)
-      yhat <<- predict(lmResults())
-      yline <<- predict(lmResults(), predictor)
+      predictor <- data.frame(x=xcon,x2=x2,x3=x3,sqrtx=sqrtx,logx=logx,expx=expx)
+      yhat <- predict(lmResults())
+      yline <- predict(lmResults(), predictor)
       plot(c(min(x),max(x))
            ,c(min(y,yline),max(y,yline)),
            type="n",
